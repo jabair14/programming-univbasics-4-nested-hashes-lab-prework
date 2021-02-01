9d8706cf8193525ce15902e4d8344d0b7e0dac5d
Jabair Khan <jabairk@gmail.com>
2021-02-01 23:04:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -123,6 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages][0]
+
 
 end
